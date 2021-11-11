e98d1f911801999687504dabac811a25863c047c
Maximilian Hansen <maximilianhansen@Maximilians-MacBook-Pro.local>
2021-11-11 18:50:06 -0500

    daily coding problem nov 11th added

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 var cards = document.querySelectorAll(".cardy");
 let i = 0;
 
-let content = [{difficulty: "Medium Twitter", title: "Nov-10th", body : "Good morning! Here's your coding interview problem for today.This problem was asked by Twitter. Implement an autocomplete system. That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix. For example, given the query string de and the set of strings \[dog, deer, deal\], return \[deer, deal]. Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries."},
+let content = [
+               {difficulty: "Easy Amazon", title: "Nov-11th", body : "Good morning! Here's your coding interview problem for today.This problem was asked by Amazon. You are given a list of data entries that represent entries and exits of groups of people into a building. An entry looks like this: \{\"timestamp\": 1526579928, count\: 3, \"type\": \"enter\"\} This means 3 people entered the building. An exit looks like this: \{\"timestamp\": 1526580382, count: 2, \"type\": \"exit\"\} This means that 2 people exited the building. timestamp is in Unix time. Find the busiest period in the building, that is, the time with the most people in the building. Return it as a pair of (start, end) timestamps. You can assume the building always starts off and ends up empty, i.e. with 0 people inside."},
+               {difficulty: "Medium Twitter", title: "Nov-10th", body : "Good morning! Here's your coding interview problem for today.This problem was asked by Twitter. Implement an autocomplete system. That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix. For example, given the query string de and the set of strings \[dog, deer, deal\], return \[deer, deal]. Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries."},
                {difficulty : "Medium Stich Fix", title: "Nov-7th" , body : "Good morning! Here's your coding interview problem for today. This problem was asked by Stitch Fix. Pascal's triangle is a triangular array of integers constructed with the following formula: The first row consists of the number 1. For each subsequent row, each element is the sum of the numbers directly above it, on either side. For example, here are the first few rows: 1 1 1 1 2 1 1 3 3 1 1 4 6 4 1 Given an input k, return the kth row of Pascal's triangle. Bonus: Can you do this using only O(k) space?"},
                {difficulty:"Easy IBM" , title : "Nov-5th", body : "Good morning! Here's your coding interview problem for today. This problem was asked by IBM. Given an integer, find the next permutation of it in absolute order. For example, given 48975, the next permutation would be 49578." },            
                {difficulty: "Easy Wayfair", title : "Nov-4th", body : "Good morning! Here's your coding interview problem for today. This problem was asked by Wayfair. You are given a 2 x N board, and instructed to completely cover the board with the following shapes: Dominoes, or 2 x 1 rectangles. Trominoes, or L-shapes. For example, if N = 4, here is one possible configuration, where A is a domino, and B and C are trominoes. A B B C A B C C Given an integer N, determine in how many ways this task is possible."},
@@ -18,7 +20,7 @@ let content = [{difficulty: "Medium Twitter", title: "Nov-10th", body : "Good mo
 
 function makeVerticalCards(card, selector){
     document.querySelector(selector).innerHTML += 
-        `<div class="card cardy m-3 bg-image hover-overlay" style="width: 18rem;">
+        `<div class="card cardy m-2 bg-image hover-overlay" style="width: 18rem;">
                   <div class = "imgSize">
                   <img src="solutions/${card.title}/Solution.jpg" class="card-img-top " alt="...">
                   </div>
@@ -31,7 +33,7 @@ function makeVerticalCards(card, selector){
                     rgba(91, 14, 214, 0.5) 100%);">
                 </div>
                 <div class="card-body">
-                  <h5 class="card-title">${card.title}</h5>
+                  <h5 class="card-title">${card.title} ${card.difficulty}</h5>
                   <p class="card-text">${card.body.slice(0,75)}</p>
                   <a href="#" class="btn btn-dark">See More</a>
                 </div>
@@ -40,7 +42,7 @@ function makeVerticalCards(card, selector){
 
 function makeHorizontalCards(card, selector){
     document.querySelector(selector).innerHTML += 
-    `<div class="card cardInGrid mb-5 bg-image hover-overlay" style="max-width: 900px;">
+    `<div class="card cardInGrid mb-5 bg-image hover-overlay" style="max-width: 1400px;">
     <div class="row g-0">
       <div class="col-md-4 ">
         <img src="solutions/${card.title}/Solution.jpg" class="img-fluid rounded-start" alt="...">
@@ -55,7 +57,7 @@ function makeHorizontalCards(card, selector){
       );"></div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">${card.title}</h5>
+          <h5 class="display-5 card-title">${card.title}  ${card.difficulty}</h5>
           <p class="card-text">${card.body}</p>
           <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
         </div>
@@ -65,8 +67,10 @@ function makeHorizontalCards(card, selector){
 };
 
 if (document.querySelector(".homePageCards")) {
-content.forEach((card)=> {makeVerticalCards(card, ".homePageCards")});}
+let i = 0;
+content.forEach((card)=> {if(i<4) {makeVerticalCards(card, ".homePageCards")} i++ });
+}
 
 if (document.querySelector(".cardsHolder")) {
 content.forEach((card) => {makeHorizontalCards(card, ".cardsHolder")});
-}
+} 
