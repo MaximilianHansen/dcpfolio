92b464ccf61911949cb312fc19dc81c70b2a40dc
Maximilian Hansen <maximilianhansen@Maximilians-MacBook-Pro.local>
2021-11-12 16:11:54 -0500

    added nov 12

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var cards = document.querySelectorAll(".cardy");
 
 let content = [
+               {difficulty: "Medium Google", title: "Nov-12th", body : "Good morning! Here's your coding interview problem for today. This problem was asked by Google. Given a stack of N elements, interleave the first half of the stack with the second half reversed using only one other queue. This should be done in-place. Recall that you can only push or pop from a stack, and enqueue or dequeue from a queue. For example, if the stack is [1, 2, 3, 4, 5], it should become [1, 5, 2, 4, 3]. If the stack is [1, 2, 3, 4], it should become [1, 4, 2, 3]. Hint: Try working backwards from the end state."},
                {difficulty: "Easy Amazon", title: "Nov-11th", body : "Good morning! Here's your coding interview problem for today.This problem was asked by Amazon. You are given a list of data entries that represent entries and exits of groups of people into a building. An entry looks like this: \{\"timestamp\": 1526579928, count\: 3, \"type\": \"enter\"\} This means 3 people entered the building. An exit looks like this: \{\"timestamp\": 1526580382, count: 2, \"type\": \"exit\"\} This means that 2 people exited the building. timestamp is in Unix time. Find the busiest period in the building, that is, the time with the most people in the building. Return it as a pair of (start, end) timestamps. You can assume the building always starts off and ends up empty, i.e. with 0 people inside."},
                {difficulty: "Medium Twitter", title: "Nov-10th", body : "Good morning! Here's your coding interview problem for today.This problem was asked by Twitter. Implement an autocomplete system. That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix. For example, given the query string de and the set of strings \[dog, deer, deal\], return \[deer, deal]. Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries."},
                {difficulty : "Medium Stich Fix", title: "Nov-7th" , body : "Good morning! Here's your coding interview problem for today. This problem was asked by Stitch Fix. Pascal's triangle is a triangular array of integers constructed with the following formula: The first row consists of the number 1. For each subsequent row, each element is the sum of the numbers directly above it, on either side. For example, here are the first few rows: 1 1 1 1 2 1 1 3 3 1 1 4 6 4 1 Given an input k, return the kth row of Pascal's triangle. Bonus: Can you do this using only O(k) space?"},
@@ -37,7 +38,7 @@ function makeHorizontalCards(card, selector, index){
     `<div class="card horzCardClick cardInGrid mb-5 bg-image hover-overlay" style="max-width: 1400px;">
     <div class="row g-0">
       <div class="col-md-4 ">
-        <img src="solutions/${card.title}/Solution.jpg" class="img-fluid rounded-start" alt="...">
+        <img src="solutions/${card.title}/Solution.jpg" class="img-fluid imgSize rounded-start" alt="...">
       </div>
       <div
       class="mask" id="${index}"
